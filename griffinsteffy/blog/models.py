f71bef23b90bc7d7535b81ff6ee0a3716d1b504d
Griffin Steffy <griffinsteffy19@gmail.com>
2022-06-21 23:13:06 -0400

    post form with hidden login, search functionality

--- griffinsteffy/blog/models.py
+++ griffinsteffy/blog/models.py
@@ -1,13 +1,20 @@
 import datetime
 from django.db import models
 from django.utils import timezone
+from taggit.managers import TaggableManager
 
 class Post(models.Model):
-    title = models.CharField(max_length=200)
+    title = models.CharField(max_length=100)
     pub_date = models.DateTimeField('date published')
     content = models.FileField(upload_to='blog/templates/posts/')
     is_featured = models.BooleanField(default=False)
+    tags = TaggableManager()
+    
+    class Meta:
+        ordering = ['title']
+    
     def __str__(self):
         return self.title
+    
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
--- griffinsteffy/blog/urls.py
+++ griffinsteffy/blog/urls.py
@@ -3,14 +3,16 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # ex: /blog/list/
+    # ex: /blog/
     path('', views.postList, name='list'),
-    # ex: /blog/5/
+    # ex: /blog/#/
     path('<int:post_id>/', views.singlePost, name='single'),
     # ex: /blog/first/
     path('first/', views.first, name='first'),
     # ex: /blog/random/
     path('random/', views.randomPost, name='random'),
-    # ex: /blog/test/
-    path('test/', views.test, name='test'),
+    # ex: /blog/search/
+    path('search/', views.search, name='search'),
+    # ex: /blog/addpost/
+    path('addpost/', views.addpost, name='addpost'),
 ]
