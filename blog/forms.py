65d6d23ac4b0692efe67e049e86520fd9ec89349
Griffin Steffy <griffinsteffy19@gmail.com>
2022-06-22 22:04:26 -0400

    move to base level

--- blog/forms.py
+++
@@ -1,7 +0,0 @@
-from django.forms import ModelForm
-from .models import Post
-
-class NewPostForm(ModelForm):
-    class Meta:
-        model = Post
-        fields= ['title', 'pub_date', 'content', 'is_featured', 'thumbnail', 'tags']
