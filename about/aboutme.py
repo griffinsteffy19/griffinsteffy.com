778370070adedb10d8a68d8b8448b5204c1aed6f
Griffin Steffy <griffinsteffy19@gmail.com>
2022-06-25 13:24:04 -0400

    fix broken links

--- about/aboutme.py
+++ about/aboutme.py
@@ -2,7 +2,7 @@ about ={
     'name': 'Griffin Steffy',
     'initials': 'GS',
     'preview': "short paragraph about me will go here",
-    'instagram': 'https://www.instagram.com/being.griffin/',
-    'youtube': 'https://www.youtube.com/channel/UCFMDZqGUa5nc3TmRJueCNEA',
-    'linkedin': 'https://www.linkedin.com/feed/?trk=homepage-basic_signin-form_submit'
+    'instagram': "https://www.instagram.com/being.griffin/",
+    'youtube': "https://www.youtube.com/channel/UCFMDZqGUa5nc3TmRJueCNEA",
+    'linkedin': "https://www.linkedin.com/feed/?trk=homepage-basic_signin-form_submit"
 }
