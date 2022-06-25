c892880a8d9208301b6c82179c8aec1f896501be
Griffin Steffy <griffinsteffy19@gmail.com>
2022-06-25 13:16:53 -0400

    first post updates

--- blog/forms.py
+++ blog/forms.py
@@ -4,4 +4,4 @@ from .models import Post
 class NewPostForm(ModelForm):
     class Meta:
         model = Post
-        fields= ['title', 'pub_date', 'content', 'is_featured', 'thumbnail', 'tags']
+        fields= ['title', 'pub_date', 'content', 'thumbnail', 'preview', 'tags','is_featured', 'featured_preview']
