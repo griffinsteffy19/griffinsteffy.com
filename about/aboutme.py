1c9fea54daf274f4b26e77661b6ed71d9f928aab
Griffin Steffy <griffinsteffy19@gmail.com>
2022-06-26 15:35:45 -0400

    about me updates

--- about/aboutme.py
+++ about/aboutme.py
@@ -1,7 +1,7 @@
 about ={
     'name': 'Griffin Steffy',
     'initials': 'GS',
-    'preview': "short paragraph about me will go here",
+    'preview': "Computer engineer by trade, wedding videographer on the side. Starting a blog to let everything else I do have a place to live.",
     'instagram': "https://www.instagram.com/being.griffin/",
     'youtube': "https://www.youtube.com/channel/UCFMDZqGUa5nc3TmRJueCNEA",
     'linkedin': "https://www.linkedin.com/feed/?trk=homepage-basic_signin-form_submit"
